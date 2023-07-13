1454bd3a282823b0f37084b00b4ebd4f35dab2ec
Tinnerson <128089943+Tinnerson@users.noreply.github.com>
2023-07-13 16:50:23 -0600

    Update main.py
    Now it can take multiple inputs (simply enter empty line to stop). Also made it so that the song stops after like 8 seconds. But turns out this doesn't work for any song not in English, which is quite the problem for my specific case. Also made everything a little bit faster.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pyautogui
 import os
 import time
+import sys
 
-songname=input("Enter list of songs: \n")
-print("Searching for " + songname)
+inputs = [] 
+input1 = input("What is the input? ") 
+while input1 != "": 
+	inputs.append(input1) 
+	input1 = input("What is the input? ")
 
-os.system(r"C:\Users\inhor\AppData\Roaming\Spotify\Spotify.exe")
-time.sleep(5)
-pyautogui.hotkey('ctrl', 'l')
-pyautogui.write(songname, interval=0.1)
 
-for key in ['enter', 'pagedown', 'tab', 'enter', 'enter']:
-    time.sleep(2)
-    pyautogui.press(key)
+for i in inputs:
+    print("Searching for " + i)
+
+    os.system(r"C:\Users\inhor\AppData\Roaming\Spotify\Spotify.exe")
+    time.sleep(1)
+    pyautogui.hotkey('ctrl', 'l')
+    pyautogui.write(i, interval=0.1)
+
+    for key in ['enter', 'pagedown', 'tab', 'enter', 'enter']:
+        time.sleep(2)
+        pyautogui.press(key)
+    time.sleep(10)
+    pyautogui.press('space')
